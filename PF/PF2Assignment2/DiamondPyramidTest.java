c39e75cdadf854e935e7ab9c7ea6cd3f9d64bc7d
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 16:04:33 +0530

    Update DiamondPyramidTest.java

--- PF/PF2Assignment2/DiamondPyramidTest.java
+++ PF/PF2Assignment2/DiamondPyramidTest.java
@@ -5,15 +5,15 @@ public class DiamondPyramidTest {
 	public void testWholePyramid() {
 		String[] result = {"    1    ","   121   ","  12321  "," 1234321 ","123454321"," 1234321 ","  12321  "
 				           ,"   121   ","    1    "};
-		DiamondPyramid d=new DiamondPyramid();
-		String[] expected=d.wholePyramid(5);
+		DiamondPyramid d = new DiamondPyramid();
+		String[] expected = d.wholePyramid(5);
 		assertArrayEquals(expected,result);
 	}
 	public void testWholePyramidForEven() {
 		String[] result = {"     1     ","    121    ","   12321   ","  1234321  "," 123454321 ","12345654321"," 123454321 ","  1234321  ","   12321   "
 				           ,"    121    ","     1     "};
-		DiamondPyramid d=new DiamondPyramid();
-		String[] expected=d.wholePyramid(6);
+		DiamondPyramid d = new DiamondPyramid();
+		String[] expected = d.wholePyramid(6);
 		assertArrayEquals(expected,result);
 	}
 	@Test
