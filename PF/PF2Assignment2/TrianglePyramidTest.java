d3b61a35e3582edea2e5f0270fee5b932e3f5a35
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 14:27:39 +0530

    Update TrianglePyramidTest.java

--- PF/PF2Assignment2/TrianglePyramidTest.java
+++ PF/PF2Assignment2/TrianglePyramidTest.java
@@ -6,21 +6,24 @@ public class TrianglePyramidTest
 	public void test() 
 	{
 		String[] result = {"12345"," 1234","  123","   12","    1"};
-		String[] expected=TrianglePyramid.wholePyramid(5);
+		TrianglePyramid t=new TrianglePyramid();
+		String[] expected=t.wholePyramid(5);
 		assertArrayEquals(expected,result);
 	}
 	@Test
 	public void test() 
 	{
 		String[] result = {"123456"," 12345","  1234","   123","    12","     1"};
-		String[] expected=TrianglePyramid.wholePyramid(6);
+		TrianglePyramid t=new TrianglePyramid();
+		String[] expected=t.wholePyramid(6);
 		assertArrayEquals(expected,result);
 	}
 	@Test
 	public void testInvalidInput() 
 	{
 		String[] result = {"invalid input"};
-		String[] expected=TrianglePyramid.wholePyramid(0);
+		TrianglePyramid t=new TrianglePyramid();
+		String[] expected=t.wholePyramid(0);
 		assertArrayEquals(expected,result);
 	}
 }
