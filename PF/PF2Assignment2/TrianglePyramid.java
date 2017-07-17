89c32f8258b949e4e8abb8a2b060d5125bf3f6d5
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 14:23:30 +0530

    Update TrianglePyramid.java

--- PF/PF2Assignment2/TrianglePyramid.java
+++ PF/PF2Assignment2/TrianglePyramid.java
@@ -1,39 +1,27 @@
-
 public class TrianglePyramid {
-
-	public static void main(String[] args) {
-		String[] result =TrianglePyramid.wholePyramid(5);
-		for(int i=0;i<5;i++){
-			System.out.println(result[i]);
-		}
-
-	}
-	public static String[] wholePyramid(int n){
+	public String[] wholePyramid(int n){
 		String[] invalid = {"invalid input"};
 		if(n<=0)
 			return invalid;
 		String[] result = new String[n];
+		TrianglePyramid t=new TrianglePyramid();
 		for(int i=0;i<n;i++){
-			result[i] = returnSpaces(n,n-i-1)+returnNumbers(n,i);
-			
+			result[i] = t.returnSpaces(n,n-i-1)+t.returnNumbers(n,i);	
 		}
 		return result;
 	}
-	public static String returnSpaces(int n, int k){
+	private String returnSpaces(int n, int k){
 		String spaces = new String();
 		for(int i=n-k-1;i>0;i--){
-			spaces = spaces + " ";
+			spaces +=" ";
 		}
 		return spaces;
 	}
-	public static String returnNumbers(int n, int k){
+	private String returnNumbers(int n, int k){
 		String numbers = new String();
-		//numbers = Integer.toString(n-k);
 		for(int i=0;i<n-k;i++){
-			numbers =  numbers+Integer.toString(i+1) ;
+			numbers += Integer.toString(i+1) ;
 		}
-		//System.out.print(numbers);
 		return numbers;
 	}
-
 }
