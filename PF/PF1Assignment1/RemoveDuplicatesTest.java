3dd933729b74bb5eb2fd89d427a3cda15c3f3f08
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 15:38:28 +0530

    Update RemoveDuplicatesTest.java

--- PF/PF1Assignment1/RemoveDuplicatesTest.java
+++ PF/PF1Assignment1/RemoveDuplicatesTest.java
@@ -1,33 +1,30 @@
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 public class RemoveDuplicatesTest {
-	
 	@Test
 	public void noElement(){
 		System.out.println("Test for no element in array");
 		int[] input = {};
-		int[] expected = RemoveDuplicates.removeDuplicates(input);
+		RemoveDuplicates r = new RemoveDuplicates();
+		int[] expected = r.removeDuplicates(input);
 		assertEquals("no  elements",expected,expected);
 	}
-	
 	@Test
 	public void singleElement(){
 		System.out.println("Test for single element in array");
 		int[] input = {5};
-		int[] expected = RemoveDuplicates.removeDuplicates(input);
+		RemoveDuplicates r = new RemoveDuplicates();
+		int[] expected = r.removeDuplicates(input);
 		assertEquals("one element",expected,expected);
 	}
-	
 	@Test
 	public void validInput(){
 		System.out.println("Test for valid array");
 		int[] input = {2,5,4,6,3,8,5,9,3,3,6,3,9,0};
-		int[] result = RemoveDuplicates.removeDuplicates(input);
+		RemoveDuplicates r = new RemoveDuplicates();
+		int[] expected = r.removeDuplicates(input);
 		int[] expected = {0,2,3,4,5,6,8,9};
 		assertArrayEquals(expected,result);
 	}
-
 }
