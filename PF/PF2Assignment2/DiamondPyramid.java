2ed627f1a00d838b0f7fde9700fc26d9d21eb046
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 14:07:21 +0530

    Update DiamondPyramid.java

--- PF/PF2Assignment2/DiamondPyramid.java
+++ PF/PF2Assignment2/DiamondPyramid.java
@@ -1,36 +1,27 @@
-
 public class DiamondPyramid {
-	public static void main(String args[]){
-		String[] result =DiamondPyramid.wholePyramid(1);
-		for(int i=0;i<2*1-1;i++){
-			System.out.println(result[i]);
-		}
-	}
-	public static String[] wholePyramid(int n){
+	public  String[] wholePyramid(int n){
 		String[] invalid = {"invalid input"};
 		if(n<=0)
 			return invalid;
 		String[] result = new String[2*n-1];
 		for(int i=0;i<n;i++){
 			result[i] = returnSpaces(n,i)+returnNumbers(n,i)+returnSpaces(n,i);
-			result[2*n-2-i] = returnSpaces(n,i)+returnNumbers(n,i)+returnSpaces(n,i);
+			result[2*n-2-i] = result[i];
 		}
 		return result;
 	}
-	public static String returnSpaces(int n, int k){
+	private  String returnSpaces(int n, int k){
 		String spaces = new String();
 		for(int i=0;i<n-k-1;i++){
-			spaces = spaces + " ";
+			spaces +=" ";
 		}
 		return spaces;
 	}
-	public static String returnNumbers(int n, int k){
-		String numbers = new String();
-		numbers = Integer.toString(k+1);
+	private  String returnNumbers(int n, int k){
+		String numbers = new String(Integer.toString(k+1));
 		for(int i=k;i>0;i--){
 			numbers = Integer.toString(i) + numbers + Integer.toString(i);
 		}
-		//System.out.print(numbers);
 		return numbers;
 	}
 }
