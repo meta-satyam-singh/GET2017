e2c35c302030a4dd9972ac308fac01755a144063
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 14:39:59 +0530

    Update LongestSequence.java

--- PF/PF1Assignment1/LongestSequence.java
+++ PF/PF1Assignment1/LongestSequence.java
@@ -1,27 +1,21 @@
 
 
 public class LongestSequence {
-
-	public static void main(String[] args)
-	{
-		
-	}
-	
-	public static int[] longestSequence(int input[])
+	public int[] longestSequence(int input[])
 	{
 		int i = 0, j = 0, k = 0, max = 0, start = 0, last = 0;
 		int length = input.length;
 		if (length==0 || length==1)
             		return input;
-		for(i=0;i<input.length-1;i++)
+		for(i=0; i<input.length-1; i++)
 		{
-			if(input[i]<input[i+1])
+			if(input[i] < input[i+1])
 			{
 				k++;
 			}
 			else
 			{
-				if(k>max)
+				if( k > max )
 				{
 					start = j;
 					last = i;
@@ -31,16 +25,16 @@ public class LongestSequence {
 				k = 0;
 			}
 		}
-		if(k>max){
+		if( k > max ){
 			start = j;
 			last = i;
 			max = k;
 		}
 		
-		int resultArr[] =new int[max+1];
-		for( i=start,k=0;i<=last;i++,k++)
+		int resultArr[] = new int[max+1];
+		for( i=start, k=0; i<=last; i++, k++)
 		{
-			resultArr[k]=input[i];
+			resultArr[k] = input[i];
 		}
 		return resultArr;
 	}
