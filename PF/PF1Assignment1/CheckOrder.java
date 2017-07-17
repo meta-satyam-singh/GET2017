185d8174a425fcb4abc2c2cfc9a712982dbd5a00
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 14:33:06 +0530

    Update CheckOrder.java

--- PF/PF1Assignment1/CheckOrder.java
+++ PF/PF1Assignment1/CheckOrder.java
@@ -1,35 +1,29 @@
 public class CheckOrder {
-
-	public static void main(String[] args) {
-		
-	}
-	public static int orderCheck(int arr[]){
+	public  int orderCheck(int arr[]){
 		int flag = 0;
-		for(int i=0;i<arr.length-1;i++){
-			if(arr[i]<=arr[i+1]){
-				flag=1;
+		for(int i=0; i<arr.length-1; i++){
+			if(arr[i] <= arr[i+1]){
+				flag = 1;
 			}
 			else{
-				flag=0;
+				flag = 0;
 				break;
 			}
 		}
 		if(flag==0){
-			for(int i=0;i<arr.length-1;i++){
-				if(arr[i]>=arr[i+1]){
-					flag=2;
+			for(int i=0; i< arr.length-1; i++){
+				if(arr[i] >= arr[i+1]){
+					flag = 2;
 				}
 				else{
-					flag=0;
+					flag = 0;
 					break;
 				}
 			}
 		}
-		if(arr[0]==arr[arr.length-1]){
-			flag=0;
+		if(arr[0] == arr[arr.length-1]){
+			flag = 0;
 		}
-		return flag;
-
+		return flag ;
 	}
-
 }
