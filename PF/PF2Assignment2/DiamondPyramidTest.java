0dc1293cc087647cb9db9dd7c34d706983e51548
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 14:16:15 +0530

    Update DiamondPyramidTest.java

--- PF/PF2Assignment2/DiamondPyramidTest.java
+++ PF/PF2Assignment2/DiamondPyramidTest.java
@@ -1,23 +1,26 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
-
 public class DiamondPyramidTest {
-
 	@Test
 	public void testWholePyramid() {
 		String[] result = {"    1    ","   121   ","  12321  "," 1234321 ","123454321"," 1234321 ","  12321  "
 				           ,"   121   ","    1    "};
-		String[] expected=DiamondPyramid.wholePyramid(5);
+		DiamondPyramid d=new DiamondPyramid();
+		String[] expected=d.wholePyramid(5);
+		assertArrayEquals(expected,result);
+	}
+	public void testWholePyramidForEven() {
+		String[] result = {"     1     ","    121    ","   12321   ","  1234321  "," 123454321 ","12345654321"," 123454321 ","  1234321  ","   12321   "
+				           ,"    121    ","     1     "};
+		DiamondPyramid d=new DiamondPyramid();
+		String[] expected=d.wholePyramid(6);
 		assertArrayEquals(expected,result);
 	}
 	@Test
 	public void testInvalidInput() {
 		String[] result = {"invalid input"};
-		String[] expected=DiamondPyramid.wholePyramid(-2);
+		DiamondPyramid d=new DiamondPyramid();
+		String[] expected=d.wholePyramid(-2);
 		assertArrayEquals(expected,result);
 	}
-
-
 }
