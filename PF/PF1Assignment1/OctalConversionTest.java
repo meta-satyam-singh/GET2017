218d257eb71ae4201d9e950ba6f1cb822c241c32
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 15:41:36 +0530

    Update OctalConversionTest.java

--- PF/PF1Assignment1/OctalConversionTest.java
+++ PF/PF1Assignment1/OctalConversionTest.java
@@ -1,31 +1,27 @@
 
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 public class OctalConversionTest {
-
 	@Test
 	public void invalidInput(){
 		System.out.println("Test for Invalid Input");
-		int Octal_Value = OctalConversion.convertBinaryToOctal(101242);
+		OctalConversion o = new OctalConversion();
+		int Octal_Value = o.convertBinaryToOctal(101242);
 		assertEquals("invalid input",-1,Octal_Value);
 	}
-	
 	@Test
 	public void checkZero(){
 		System.out.println("Test for zero input");
-		int Octal_Value = OctalConversion.convertBinaryToOctal(000);
+		OctalConversion o = new OctalConversion();
+		int Octal_Value = o.convertBinaryToOctal(000);
 		assertEquals("input 0",0,Octal_Value);
-	}
-	
+	}	
 	@Test
 	public void validInput(){
 		System.out.println("Test for valid Input");
-		int Octal_Value = OctalConversion.convertBinaryToOctal(110101);
+		OctalConversion o = new OctalConversion();
+		int Octal_Value = o.convertBinaryToOctal(110101);
 		assertEquals("valid input",65,Octal_Value);
 	}
-	
-
 }
