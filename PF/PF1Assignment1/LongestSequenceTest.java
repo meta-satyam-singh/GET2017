6bd22c130b4e4d49f4b38fd432f55f73078b7766
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 15:47:07 +0530

    Update LongestSequenceTest.java

--- PF/PF1Assignment1/LongestSequenceTest.java
+++ PF/PF1Assignment1/LongestSequenceTest.java
@@ -4,31 +4,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LongestSequenceTest {
-
 	@Test
 	public void noElement(){
 		System.out.println("Test for no element in array");
 		int[] input = {};
-		int[] expected = LongestSequence.longestSequence(input);
+		LongestSequence l = new LongestSequence();
+		int[] expected = l.longestSequence(input);
 		assertEquals("no  elements",input,expected);
 	}
-	
 	@Test
 	public void singleElement(){
 		System.out.println("Test for single element in array");
 		int[] input = {5};
-		int[] expected = LongestSequence.longestSequence(input);
+		LongestSequence l = new LongestSequence();
+		int[] expected = l.longestSequence(input);
 		assertEquals("one element",input,expected);
 	}
-	
 	@Test
 	public void validInput(){
 		System.out.println("Test for valid array");
 		int[] input = {1,2,3,2,3,4,5,3,4,2,2,3,4,5,6,7,8,1,2,4,5,6,7,8,9};
-		int[] result = LongestSequence.longestSequence(input);
+		LongestSequence l = new LongestSequence(); 
+		int[] result = l.longestSequence(input);
 		System.out.println(result.length);
 		int[] expected = {1, 2,4,5,6,7,8,9};
 		assertArrayEquals(expected,result);
 	}
-
 }
