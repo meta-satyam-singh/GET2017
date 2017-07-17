35b32a377851ea82445232af4ac43b87cb091a32
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-17 10:48:54 +0530

    Solution of Assignment 2

---
+++ PF/PF2Assignment2/DiamondPyramidTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DiamondPyramidTest {
+
+	@Test
+	public void testWholePyramid() {
+		String[] result = {"    1    ","   121   ","  12321  "," 1234321 ","123454321"," 1234321 ","  12321  "
+				           ,"   121   ","    1    "};
+		String[] expected=DiamondPyramid.wholePyramid(5);
+		assertArrayEquals(expected,result);
+	}
+	@Test
+	public void testInvalidInput() {
+		String[] result = {"invalid input"};
+		String[] expected=DiamondPyramid.wholePyramid(-2);
+		assertArrayEquals(expected,result);
+	}
+
+
+}
