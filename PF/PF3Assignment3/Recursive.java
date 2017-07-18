3db30462215e9ce3450f706ac1dbed9ced6af089
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-18 11:47:30 +0530

    Update Recursive.java

--- PF/PF3Assignment3/Recursive.java
+++ PF/PF3Assignment3/Recursive.java
@@ -1,14 +1,11 @@
 package PF3;
-
 public class Recursive {
-
 	public static void main(String[] args) {
 		Recursive k=new Recursive();
 		System.out.println(k.rem(5, 2));
 		System.out.println(k.gcd(1,2));
 		System.out.println(k.largestDigit(2));
 	}
-	
 	// takes dividend and divisor and returns remainder
 	public static int rem(int dividend, int divisor){
 		if(divisor == 0)
@@ -20,7 +17,6 @@ public class Recursive {
 			return k;
 		return rem(k,divisor);
 	}
-	
 	//takes two numbers as argument and returns their gcd
 	public static int gcd ( int x , int y )
 	{
@@ -30,7 +26,6 @@ public class Recursive {
 	        return gcd ( y , x % y );
 	    else return gcd ( y , x );        // if x < y then go ahead and switch them around.
 	}
-	
 	// takes number as argument and returns largest digit
 	public static int largestDigit(int num){
 		if(num<10) return num;
