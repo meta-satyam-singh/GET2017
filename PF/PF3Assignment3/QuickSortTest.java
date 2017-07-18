9966ceb3be05eb25029333f028dd7e5d2cf41844
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-18 11:44:03 +0530

    Update QuickSortTest.java

--- PF/PF3Assignment3/QuickSortTest.java
+++ PF/PF3Assignment3/QuickSortTest.java
@@ -1,18 +1,14 @@
 package PF3;
-
 import static org.junit.Assert.*;
 import org.junit.Test;
-
-public class QuickSortTest {
-	
+public class QuickSortTest {	
 	@Test
 	public void noElement(){
 		int[] input = {};
 		QuickSort q = new QuickSort();
 		int[] expected = q.sort(input,0,0);
 		assertEquals("no  elements",expected,input);
-	}
-	
+	}	
 	@Test
 	public void validInput(){
 		int[] input = {2,5,8,9,10, 77, 55, 11};
