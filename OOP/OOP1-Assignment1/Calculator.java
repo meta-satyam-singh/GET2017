57766e1e4b08e01916ef8fa75d3c6e46a4d1e841
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-20 13:21:26 +0530

    Solution Of OOP Assignment1

---
+++ OOP/OOP1-Assignment1/Calculator.java
@@ -0,0 +1,23 @@
+package OOP1;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Calculator implements ActionListener {
+	
+		Digits[] digit;
+		Operator[] operator;
+		Layout layout;
+		Display display;
+		
+		
+		public Calculator(int digit, int operator) {
+			//initialize all the objects
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			//logic for action when the buttons are clicked
+		}
+		
+}
---
+++ OOP/OOP1-Assignment1/Layout.java
@@ -0,0 +1,22 @@
+package OOP1;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Layout extends JFrame{
+	
+	public Layout() {
+		//Initialize the variables
+	}
+	
+	public void add(JComponent component) {
+		//Adding the component to the layout
+	}
+	
+	public void setLayout(LayoutManager layout) {
+		//set the type of layout
+	}
+}
