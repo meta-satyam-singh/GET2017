4fdd7a45b5f93966a5fb180b1d2aab23305d5963
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-20 14:03:12 +0530

    Update RecursiveSearch.java

--- PF/PF3Assignment3/RecursiveSearch.java
+++ PF/PF3Assignment3/RecursiveSearch.java
@@ -1,7 +1,12 @@
 package PF3;
 public class RecursiveSearch {
 	// takes array, number to search and index as argument and returns boolean
-	public boolean linearSearch(int arr[], int num, int i){
+	public boolean linearSearch(int arr[], int num)
+	{
+		return linearSearchRec( arr, num , 0);
+	}
+	private boolean linearSearchRec(int arr[], int num, int i)
+	{
 		
 		if(i >= arr.length)
 			return false;
@@ -9,20 +14,26 @@ public class RecursiveSearch {
 			return true;
 	
 		else 
-			return linearSearch(arr, num,i+1);
+			return linearSearchRec(arr, num,i+1);
 	}
 	/* takes array and it's leftmost and rightmost index and the
 	 *  number to search as argument and returns boolean
 	 */
-	public boolean binarySearch(int arr[], int left, int right, int num){
-		if(right >= left){
-		int mid = left  + (right -left)/2;
-		if(arr[mid] == num)
-			return true;
-		else if(arr[mid] > num)
-			return binarySearch(arr,left,mid-1,num);
-		else 
-			return binarySearch(arr,mid+1,right,num);
+	public boolean binarySearch(int arr[], int left, int right, int num)
+	{
+		return binarySearchRec( arr, 0, arr.length-1, num) ;
+	}
+	private boolean binarySearchRec(int arr[], int left, int right, int num)
+	{
+		if(right >= left)
+		{
+			int mid = left  + (right -left)/2;
+			if(arr[mid] == num)
+				return true;
+			else if(arr[mid] > num)
+				return binarySearchRec(arr,left,mid-1,num);
+			else 
+				return binarySearchRec(arr,mid+1,right,num);
 		}
 		return false;
 	}
