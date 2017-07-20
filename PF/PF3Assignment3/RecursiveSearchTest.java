c8ca570d443ffaf99eb4f88e0f6f87717ed6728b
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-20 14:04:04 +0530

    Update RecursiveSearchTest.java

--- PF/PF3Assignment3/RecursiveSearchTest.java
+++ PF/PF3Assignment3/RecursiveSearchTest.java
@@ -16,24 +16,21 @@ public class RecursiveSearchTest {
 	public void testLinearSearch2() {
 		int[] input = {2,5,8,9,10,77,55};
 		boolean result = true;
-		RecursiveSearch r = new RecursiveSearch();
-		boolean expected = r.linearSearch(input, 77, 0);
+		boolean expected = new RecursiveSearch().linearSearch(input, 77, 0);
 		assertEquals(expected, result);
 	}
 	@Test
 	public void testBinarySearch1() {
 		int[] input = {2,5,8,9,10,55,77};
 		boolean result = false;
-		RecursiveSearch r = new RecursiveSearch();
-		boolean expected = r.binarySearch(input,0,input.length-1,88);
+		boolean expected =  new RecursiveSearch().binarySearch(input,0,input.length-1,88);
 		assertEquals(expected, result);
 	}
 	@Test
 	public void testBinarySearch2() {
 		int[] input = {2,5,8,9,10,55,77};
 		boolean result = true;
-		RecursiveSearch r = new RecursiveSearch();
-		boolean expected = r.binarySearch(input,0,input.length-1,77);
+		boolean expected =  new RecursiveSearch().binarySearch(input,0,input.length-1,77);
 		assertEquals(expected, result);
 	}
 }
