076fcd0836ea901369297d8b02f03ff24a1c1e32
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-20 13:48:56 +0530

    Update QuickSort.java

--- PF/PF3Assignment3/QuickSort.java
+++ PF/PF3Assignment3/QuickSort.java
@@ -3,7 +3,7 @@ public class QuickSort {
 	public int partition(int arr[], int low, int high)
 	{
 		int pivot = arr[high]; 
-		int i = (low-1); 
+		int i = low-1; 
 		for (int j=low; j<high; j++)
 		{
 			if (arr[j] <= pivot)
