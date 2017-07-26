1e12c6cc586f9749e05c8fc49d8099963c5851ce
meta-satyam-singh <satyam.singh@metacube.com>
2017-07-26 12:45:28 +0530

    Update OrderPromotion.java

--- OOP/OOP1-Assignment3/OrderPromotion.java
+++ OOP/OOP1-Assignment3/OrderPromotion.java
@@ -2,46 +2,55 @@ package oop3;
 
 import java.util.ArrayList;
 
-public class OrderPromotion {
+public class OrderPromotion 
+{
 	private String promotionType;
 	private String discount;
 	private String threshold;
 	
 	// getters and setters fpr all fields of order promotion
-	public String getPromotionType(){
+	public String getPromotionType()
+	{
 		return promotionType;
 	}
-	public void setPromotionType(String promotionType){
+	public void setPromotionType(String promotionType)
+	{
 		this.promotionType = promotionType;
 	}
-	public String getDiscount(){
+	public String getDiscount()
+	{
 		return discount;
 	}
-	public void setDiscount(String discount){
+	public void setDiscount(String discount)
+	{
 		this.discount = discount;
 	}
 	public String getThreshold(){
 		return threshold;
 	}
-	public void setThreshold(String threshold){
+	public void setThreshold(String threshold)
+	{
 		this.threshold = threshold;
 	}
 	
 	// calculates discount on order
-	public float orderDiscount(OrderPromotion[] orderObj, float subTotal, ArrayList<String> report) {
+	public float orderDiscount(OrderPromotion[] orderObj, float subTotal, ArrayList<String> report) 
+	{
 		float discount = 0;
 		int threshold = 0;
 		
 		// checks for all order promotions
-		for (int i = 0; i < orderObj.length; i++) {
+		for (int i = 0; i < orderObj.length; i++) 
+		{
 			
 			// checks if threshold of promotion is less than total of order
-			if (Float.parseFloat(orderObj[i].getThreshold()) <= subTotal) {
+			if (Float.parseFloat(orderObj[i].getThreshold()) <= subTotal) 
+			{
 				boolean flagForThres = false;
 				float temp;
 				// calculates discount for each type
-				if (orderObj[i].getPromotionType().equalsIgnoreCase(
-						"OrderFixedPercentPromotion")) {
+				if (orderObj[i].getPromotionType().equalsIgnoreCase("OrderFixedPercentPromotion")) 
+				{
 					temp = (subTotal * Float.parseFloat(orderObj[i]
 							.getDiscount())) / 100;
 					flagForThres = true;
