517d62815ec1ebc97bed66573cf3ea7f3667f6e6
meta-satyam-singh <satyam.singh@metacube.com>
2017-08-11 14:45:44 +0530

    Solution Of JavaScript Assignment3

---
+++ JavaScript/JavaScript3/Sum.js
@@ -0,0 +1,15 @@
+var sum=function(a)
+{
+	if(arguments.length == 2)
+	{
+		document.getElementById("sum").innerHTML = arguments[0] + arguments[1];
+	}
+	else if(arguments.length ==1)
+	{
+		return function(b)
+		{
+		document.getElementById("sum2").innerHTML = a+b;
+		}
+	}
+}
+
