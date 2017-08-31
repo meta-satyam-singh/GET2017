3f8d5fdbada86a1393d86f8a96358a7907cba97a
meta-satyam-singh <satyam.singh@metacube.com>
2017-08-31 14:04:23 +0530

    Update Node.java

--- DataStructure8/assignment_1/Node.java
+++ DataStructure8/assignment_1/Node.java
@@ -1,35 +1,29 @@
 package assignment_1;
 
-public class Node {
-	
+public class Node 
+{
 	// variable to define node value
 	public int nodeValue;
 	
 	// variable to define right and left pointer of tree
 	public Node left , right;
 
-	public Node(){
-		
+	public Node()
+	{
 		nodeValue = 0;
-		
 		left = null;
-		
 		right = null;
-		
 	}
 	
-	public Node(int element){
-		
+	public Node(int element)
+	{
 		nodeValue = element;
-		
 		left = null;
-		
 		right = null;
 	}
 	
-	public String toString(){
-		
+	public String toString()
+	{
 		return String.valueOf(nodeValue);
 	}
-
 }
